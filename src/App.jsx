bca0e1de8bbeeabc702897ce192cebc02923d894
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-27 15:18:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { tasks } from "./data/tasks";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>Task Manager</h1>
+			<div className="card">
+				<p></p>
+			</div>
+		</>
+	);
+}
+
+export default App;
