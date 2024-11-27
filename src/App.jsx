a5bf97a9e951cfe685a16c956a02b97b5e49ba71
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-27 16:12:56 +0100

    liste divise tra current e completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import { tasks } from "./data/tasks";
 import "./App.css";
 
 function App() {
-	const tasksList = () => {
-		return tasks.map((task, index) => <li key={task.id}>{task.title}</li>);
+	//LISTA DI TASK DA COMPLETARE
+	const filterCurrentTasks = () => {
+		return tasks.filter(
+			(task) => task.state == "backlog" || task.state == "in_progress"
+		);
+	};
+	const currentTasksList = filterCurrentTasks();
+	const uncompletedTasksList = () => {
+		return currentTasksList.map((task, index) => (
+			<li key={task.id}>{task.title}</li>
+		));
+	};
+
+	//LISTA DI TASK COMPLETATE
+	const filterCompletedTasks = () => {
+		return tasks.filter((task) => task.state == "completed");
+	};
+	const completedTasksList = filterCompletedTasks();
+	const tasksCompletedList = () => {
+		return completedTasksList.map((task, index) => (
+			<li key={task.id}>{task.title}</li>
+		));
 	};
 	return (
 		<>
 			<h1>Task Manager</h1>
-			<p>{tasksList()}</p>
+			<h2>Current Tasks</h2>
+			<p>{uncompletedTasksList()}</p>
+			<h2>Completed Tasks</h2>
+			<p>{tasksCompletedList()}</p>
 		</>
 	);
 }
